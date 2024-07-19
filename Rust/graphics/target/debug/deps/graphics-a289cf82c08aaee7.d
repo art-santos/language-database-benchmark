26612fd134c2b919c1111a225a57760e0cea243f
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-07-19 19:41:25 -0300

    feat: Add Rust code for graphics and wasm examples

---
+++ Rust/graphics/target/debug/deps/graphics-a289cf82c08aaee7.d
@@ -0,0 +1,5 @@
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/graphics/target/debug/deps/libgraphics-a289cf82c08aaee7.rmeta: src/main.rs
+
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/graphics/target/debug/deps/graphics-a289cf82c08aaee7.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/graphics/target/debug/deps/graphics-d01d448fd9cc9ad7.d
@@ -0,0 +1,5 @@
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/graphics/target/debug/deps/libgraphics-d01d448fd9cc9ad7.rmeta: src/main.rs
+
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/graphics/target/debug/deps/graphics-d01d448fd9cc9ad7.d: src/main.rs
+
+src/main.rs:
