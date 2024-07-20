760ae072858fe1bae8a136f9e0cfee3cd833171c
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-07-20 13:46:07 -0300

    feat: Add Rust calculator project

---
+++ Rust/calculator/target/debug/deps/calculator-2f2c9e5d80a785eb.d
@@ -0,0 +1,5 @@
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/calculator/target/debug/deps/libcalculator-2f2c9e5d80a785eb.rmeta: src/main.rs
+
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/calculator/target/debug/deps/calculator-2f2c9e5d80a785eb.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/calculator/target/debug/deps/calculator-c360d65695b641b2.d
@@ -0,0 +1,5 @@
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/calculator/target/debug/deps/libcalculator-c360d65695b641b2.rmeta: src/main.rs
+
+/Users/arthursantos/Desktop/learning/ruby/database-benchmark/Rust/calculator/target/debug/deps/calculator-c360d65695b641b2.d: src/main.rs
+
+src/main.rs:
