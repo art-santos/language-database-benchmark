69958921f43319982bc3125c0d87d0309d2be068
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-07-24 19:56:00 -0300

    chore: Update main.rs to use async main function with tokio

--- Rust/rust-mongo/src/main.rs
+++ Rust/rust-mongo/src/main.rs
@@ -1,7 +1,11 @@
 use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
 use std::env;
+use std::error::Error;
+use tokio;
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
    // Load the MongoDB connection string from an environment variable:
    let client_uri =
       env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
