c30d896e6dd5d985b72553d839cda28826be04d9
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-07-26 17:44:18 -0300

    chore: Update main.rs to use async main function with tokio

--- Rust/rust-mongo/src/main.rs
+++ Rust/rust-mongo/src/main.rs
@@ -1,16 +1,26 @@
-use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
+use mongodb::{ 
+	bson::{Document, doc},
+	Client,
+	Collection 
+};
 use std::env;
-use std::error::Error;
-use tokio;
-
 
 #[tokio::main]
-async fn main() {
-   // Load the MongoDB connection string from an environment variable:
-   let client_uri =
-      env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+async fn main() -> mongodb::error::Result<()> {
+    // Replace the placeholder with your Atlas connection string
+   let uri = env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
 
-   println!("Connecting to MongoDB at: {}", client_uri);
+    // Create a new client and connect to the server
+    let client = Client::with_uri_str(uri).await?;
 
+    // Get a handle on the movies collection
+    let database = client.database("sample_mflix");
+    let my_coll: Collection<Document> = database.collection("movies");
 
+    // Find a movie based on the title value
+    let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }).await?;
+
+    // Print the document
+    println!("Found a movie:\n{:#?}", my_movie);
+    Ok(())
 }
